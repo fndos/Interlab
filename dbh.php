f3b224f3d00c2079dae96044e8af31f83812f72b
fndos <fndos@hotmail.com>
2017-06-09 00:44:52 -0500

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -1,6 +1,7 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "interlab");
+//$conn = mysqli_connect("localhost", "root", "", "interlab");
+$conn = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "b66c7419282515", "356d4f0f", "heroku_1ea109cdfa054b7");
 
 if (!$conn) {
 	die("Connection falied: ".mysqli_connect_error());
